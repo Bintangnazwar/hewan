1252c4137707f23213cac1b1af96d7d5020c5afc
bintangnazwar <bintangnm403@gmail.com>
2024-03-04 15:14:32 +0700

    first commit

---
+++ singa.java
@@ -0,0 +1,24 @@
+public class singa {
+    public static void main(String[] args) {
+
+        System.out.println("#''#");
+        System.out.println("---");
+
+
+        String name = "singa";
+        int nomor;
+        String alamat;
+        int order;
+        int umur;
+
+
+
+        System.out.println("name:  singa ");
+        System.out.println("nomor: 087");
+        System.out.println("orde: 5");
+        System.out.println("umur: 13 tahun");
+
+
+    }
+
+}
